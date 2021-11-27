14f25fb564d3a1f06261d5c702e642f8f17b5c54
Ruslan <yaarusik@gmail.com>
2021-11-28 00:32:20 +0500

    refactor: rename js files

--- migration-typescript/src/components/controller/appLoader.ts
+++ migration-typescript/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'b4d44c5499ce43c4820fa04a9598f1f0', // получите свой ключ https://newsapi.org/
         });
     }
 }

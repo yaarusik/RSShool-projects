3db65ccb72ea140375ec133a0dc0e9953fec9ea5
Ruslan <yaarusik@gmail.com>
2021-12-04 18:37:46 +0500

    refactor: change url

--- migration-typescript/src/components/controller/appLoader.ts
+++ migration-typescript/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super(' https://nodenews.herokuapp.com/', {
             apiKey: 'b4d44c5499ce43c4820fa04a9598f1f0', // получите свой ключ https://newsapi.org/
         });
     }
